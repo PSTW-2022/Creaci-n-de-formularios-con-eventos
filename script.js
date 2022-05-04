3b79aa8385e78ce1aa107eab835585dc011c7782
Alessandro231 <aaristizabal@unal.edu.co>
2022-05-04 16:18:06 -0500

    Se crearon funciones para los checkbox

--- script.js
+++ script.js
@@ -12,6 +12,39 @@ function rojo2 (){
     document.getElementById("inputPassword2").style.borderColor = "red";
 }
 
+/*Funcion para validar las contrasenas*/
+function validarPassword(value){
+  var p1 = document.getElementById("inputPassword").value;
+  var p2 = document.getElementById("inputPassword2").value;
+  /*Para ver que no tenga espacios en blanco*/
+  var espacios = false;
+  var cont = 0;
+  
+  while (!espacios && (cont < p1.length)) {
+    if (p1.charAt(cont) == " ")
+      espacios = true;
+    cont++;
+  }
+     
+  if (espacios) {
+    alert ("La contraseña no puede contener espacios en blanco");
+    
+  }
+  /*Que contenga algo el input*/
+  if (p1.length == 0 || p2.length == 0) {
+      alert("Los campos de la password no pueden quedar vacios");
+      
+    }
+  /*Que ambas contrasenas sean iguales*/
+    if (p1 != p2) {
+      alert("Las passwords deben de coincidir");
+      
+    } else {
+      alert("Todo esta correcto");
+      return true; 
+    }
+  }
+
 /*Funcion para validar email*/
 
 var boton =  document.getElementById("boton1");
@@ -30,35 +63,25 @@ function validarEmail (value){
          alert('email valido');
 	}
 
-/*Funcion para validar las contrasenas*/
-function validarPassword(value){
-var p1 = document.getElementById("inputPassword").value;
-var p2 = document.getElementById("inputPassword2").value;
-/*Para ver que no tenga espacios en blanco*/
-var espacios = false;
-var cont = 0;
 
-while (!espacios && (cont < p1.length)) {
-  if (p1.charAt(cont) == " ")
-    espacios = true;
-  cont++;
+  /*Funcion para los checkbox*/
+  function mostrar() {
+    element = document.getElementById("checkboxtodos");
+    check = document.getElementById("flexRadioDefault1");
+    if (check.checked) {
+        element.style.display='block';
+    }
+    else {
+        element.style.display='none';
+    }
 }
-   
-if (espacios) {
-  alert ("La contraseña no puede contener espacios en blanco");
-  
-}
-/*Que contenga algo el input*/
-if (p1.length == 0 || p2.length == 0) {
-    alert("Los campos de la password no pueden quedar vacios");
-    
+function noMostrar() {
+  element = document.getElementById("checkboxtodos");
+  check = document.getElementById("flexRadioDefault2");
+  if (check.checked) {
+      element.style.display='none';
   }
-/*Que ambas contrasenas sean iguales*/
-  if (p1 != p2) {
-    alert("Las passwords deben de coincidir");
-    
-  } else {
-    alert("Todo esta correcto");
-    return true; 
+  else {
+      element.style.display='block';
   }
 }
